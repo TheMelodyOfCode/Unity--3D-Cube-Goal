429976581230ec960384765b7038430feecd6d7d
D.Cameron <lobbygow@posteo.de>
2023-05-29 12:43:52 +0200

    feat: add startView (ui), endView(ui) build and Time.DeltaTim for FPS compensation

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,11 +6,15 @@ public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody rb;
     public float speed = 0.01f;
+    public float jumpForce = 7f;
+    public bool isGrounded = true;
 
+
+    Vector3 startPos;
     // Start is called before the first frame update
     void Start()
     {
-        
+        startPos = transform.position;
     }
 
     // Update is called once per frame
@@ -19,12 +23,26 @@ public class PlayerMovement : MonoBehaviour
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
-        rb.MovePosition( rb.position + (new Vector3(x, 0, z) * speed) );
+        rb.MovePosition( rb.position + (new Vector3(x, 0, z) * speed * Time.deltaTime) );
+
+        if(transform.position.y < -15) 
+        {             
+            transform.position = startPos;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded == true )
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isGrounded = false;
+        }
     }
 
     private void OnCollisionEnter(Collision collision)
     {
        Debug.Log("Goal - Success" + collision.transform.name);
+
+        isGrounded = true;
+
         if (collision.transform.tag == "Goal")
         {
             Destroy(collision.gameObject);
---
+++ Assets/Scripts/StartView.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartView : MonoBehaviour
+{
+    public int currentLevelIndex = 0;
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
